650c1a96f96dfdb65c8a9b361cfca7636a1b3666
Web-Cookie <lishevl@gmail.com>
2021-01-01 16:44:55 +0000

    Category Queries Added

--- products/views.py
+++ products/views.py
@@ -18,6 +18,13 @@ def all_products(request):
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
